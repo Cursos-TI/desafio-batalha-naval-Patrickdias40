c187e9cc65fd4697c38ca60ca11aa63214d6f15c
Patrickdias40 <86141663+Patrickdias40@users.noreply.github.com>
2025-08-19 18:52:44 +0000

    Nivel Novato: Concluido

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,10 +5,75 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+
+    int Tabela[10][10];
+    int Navio0l;
+    int Navio0c;
+    int Navio1l;
+    int Navio1c;
+    
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
+            Tabela[i][j] = 0;
+        };
+    };
+
+    //Coluna alfabetica(margem)
+    printf("    ");
+    for (int i = 0; i < 10; i++){
+        printf(" %c ", 'A' + i);
+    }
+
+    printf("\n");
+    
+    for (int i = 0; i < 10; i++){
+        //Coluna de numeros(margem)
+        printf("%d | ", i + 1);
+
+        for (int j = 0; j < 10; j++){
+            printf(" %d ", Tabela[i][j]);
+        };
+        printf("\n");
+    };
+    
+
+    //Pedindo ao usuario a coordenadas dos navios
+    printf("\nDigite a coordenada do navio 1: \n");
+    printf("Linha:");
+    scanf("%d", &Navio0l);
+    printf("Coluna:");
+    scanf("%d", &Navio0c);
+    printf("\nDigite a coordenada do navio 2: \n");
+    printf("Linha:");
+    scanf("%d", &Navio1l);
+    printf("Coluna:");
+    scanf("%d", &Navio1c);
+    
+    for (int j = 0; j < 3; j++){
+        Tabela[Navio0l + j][Navio0c] = 3;
+    };
+
+    for (int j = 0; j < 3; j++){
+        Tabela[Navio1l][Navio1c + j] = 3;
+    };
+
+    //Coluna alfabetica(margem)
+    printf("    ");
+    for (int i = 0; i < 10; i++){
+        printf(" %c ", 'A' + i);
+    }
+
+    printf("\n");
+    
+    for (int i = 0; i < 10; i++){
+        //Coluna de numeros(margem)
+        printf("%d | ", i + 1);
+
+        for (int j = 0; j < 10; j++){
+            printf(" %d ", Tabela[i][j]);
+        };
+        printf("\n");
+    };
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
